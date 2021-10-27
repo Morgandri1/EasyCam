f3bbb82bedf732dddb9c7ca5e5ce38a89b6dc5c1
Morgan Metz <68765538+Morgandri1@users.noreply.github.com>
2021-10-27 09:04:57 -0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,61 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+faceCascade = cv2.CascadeClassifier("lib.xml")
+
+import socket
+
+# take the server name and port name
+host = 'localhost'
+port = 8080
+
+# create a socket at server side
+# using TCP / IP protocol
+s = socket.socket(socket.AF_INET,
+				socket.SOCK_STREAM)
+
+# bind the socket with server
+# and port number
+s.bind(('', port))
+
+# allow maximum 1 connection to
+# the socket
+s.listen(1)
+
+# wait till a client accept
+# connection
+c, addr = s.accept()
+
+# display client address
+print("CONNECTION FROM:", str(addr))
+
+while(True):
+	# Capture frame-by-frame
+	ret, frame = cap.read()
+
+	# Our operations on the frame come here
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	# Detect faces in the image
+	faces = faceCascade.detectMultiScale(
+		gray,
+		scaleFactor=1.1,
+		minNeighbors=5,
+		minSize=(30, 30)
+		)
+
+	data = print("{0} faces!".format(len(faces)))
+	#add new commands here 
+	c.send(b"found a face!")
+
+	for (x, y, w, h) in faces:
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+
+	cv2.imshow('frame', frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
